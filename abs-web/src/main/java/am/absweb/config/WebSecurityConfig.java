0904a3197270e807e31a5645740032e02662e31b
Artash <kinex.cssold@gmail.com>
2020-09-02 11:49:21 +0400

    Delete Templ

---
+++ abs-web/src/main/java/am/absweb/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package am.absweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .formLogin()
+//                .loginPage("/loginPage")
+//                .loginProcessingUrl("/perform_login")
+//                .defaultSuccessUrl("/successLogin")
+//                .and()
+//                .exceptionHandling().accessDeniedPage("/403")
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/")
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/").permitAll();
+    }
+//}
---
+++ abs-web/src/main/java/am/absweb/controller/MainController.java
@@ -0,0 +1,18 @@
+package am.absweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+}
